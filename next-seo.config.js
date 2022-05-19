2b6d8ab19279df60d2a173137a578e03ae99006e
Michydev <michyaraque@gmail.com>
2022-05-19 16:47:45 +0200

    feat: :mag: add next-seo to handle seo config globally and through any page

---
+++ next-seo.config.js
@@ -0,0 +1,27 @@
+/** @type {import('next-seo').Array<DefaultSeoProps>} */
+const defaultSEOConfig = {
+  title: "Michyaraque 💡",
+  titleTemplate: "%s - Michael Araque",
+  defaultTitle: "Entusiasta blockchain, backend dev con un poco de frontend 👷‍♀️",
+  description: "Crea, construye y disfruta, recuerda que lo díficil solo cuesta un poco más",
+  canonical: "https://michyaraque.dev",
+  openGraph: {
+    url: "https://michyaraque.dev",
+    title: "Michael Araque's Blog",
+    description: "Crea, construye y disfruta, recuerda que lo díficil solo cuesta un poco más",
+    images: [
+      {
+        url: "",
+        alt: "Michael Araque's Blog",
+      },
+    ],
+    site_name: "michyaraque.dev",
+  },
+  twitter: {
+    handle: "@michyaraque",
+    site: "@michyaraque",
+    cardType: "summary_large_image",
+  }
+};
+
+export default defaultSEOConfig;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,15 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { DefaultSeo } from 'next-seo'
+import defaultSEOConfig from '../../next-seo.config';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <DefaultSeo {...defaultSEOConfig} />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
