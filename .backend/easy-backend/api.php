005856a921c2d04735e4886b4ca7b771433ebb54
Michydev <michyaraque@gmail.com>
2022-05-22 06:57:38 +0200

    feat: :boom: add cors exception to the easy backend

--- .backend/easy-backend/api.php
+++ .backend/easy-backend/api.php
@@ -1,9 +1,19 @@
 <?php
 require "ApiController.php";
 
-/* header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Origin: https://michyaraque.dev');
 header('Access-Control-Allow-Methods: GET, POST');
-header("Access-Control-Allow-Headers: X-Requested-With"); */
+header("Access-Control-Allow-Headers: Content-Type");
+
+$same_site = '/https:\/\/(w{3}|w*).?michyaraque\.dev/';
+if(!preg_match($same_site, $_SERVER['HTTP_REFERER']) == 1){
+    $controller = new ApiController;
+    $controller->json([
+        'ok' => false,
+        'description' => 'called by different origin'
+    ]);
+    return $controller->sendStatus(404);
+}
 
 ApiController::get('/views/:slug', function ($req, $res) use ($redis) {
     $slug = $req->query->slug;
