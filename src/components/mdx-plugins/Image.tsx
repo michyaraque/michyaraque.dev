2ab4efd4f88728103799e2c1111807df5daccf5c
Michydev <michyaraque@gmail.com>
2022-09-11 20:00:57 +0200

    fix: :ambulance: image component for mdx problem using next/image

--- src/components/mdx-plugins/Image.tsx
+++ src/components/mdx-plugins/Image.tsx
@@ -1,10 +1,9 @@
 import React from 'react'
-import Image from 'next/image'
 
 const ImageMDX = (props: any) => {
   return (
     <div className="w-full relative select-none pointer-events-none flex">
-      <Image objectFit="cover" {...props} />
+      <img {...props} />
     </div>
   )
 }
